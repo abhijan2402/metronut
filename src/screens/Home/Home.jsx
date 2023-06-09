38c0cf4a3d204910c752cb5cce2edf5b9cfa8cd8
abhijan2402 <abhishek.jangid643@gmail.com>
2023-06-10 01:21:01 +0530

    Adding lotties animation

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react'
 // import Form from '../FormModal/Form'
 import './Home.css'
 import Navbar from '../../components/navbar/Navbar'
+import Lottie from "lottie-react";
+import confuse from '../../assets/confuse.json'
 function Home() {
     const [FormActive, setFormActive] = useState(false)
     return (
 
         <>
-        <Navbar />
-        <div>Hello</div>
-        {/* // <div className='HomeMainDiv'>
+            <Navbar />
+            <Lottie animationData={confuse} style={{ width: "10%", height: "10%" }} />
+            <div>Hello</div>
+            {/* // <div className='HomeMainDiv'>
         //     {
         //         FormActive ?
         //             // <Form /> : null
