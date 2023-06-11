5ba5ea5b3bece3ff6c59f9efe5d6a9aa092a092d
mayank-pandey-dev <130107408+mayank-pandey-dev@users.noreply.github.com>
2023-06-11 12:31:20 +0530

    update

---
+++ src/components/works/Works.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Work.css'
+
+const Works = () => {
+  return (
+    <div className='container mt-5'>
+        <h1 className='text-center pt-5' style={{fontSize: '3rem', fontWeight: '700'}}>How <span style={{fontSize: '3rem', fontWeight: '700', color: '#ff6600'}}>MentorNut</span>  MentorShip Works</h1>
+
+        <div className=''>
+
+        </div>
+    </div>
+  )
+}
+
+export default Works
