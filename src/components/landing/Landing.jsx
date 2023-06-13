2bbb981782876aad35985b6083784150b8a120de
mayank-pandey-dev <130107408+mayank-pandey-dev@users.noreply.github.com>
2023-06-13 15:46:13 +0530

    landing responsive

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
       <h1>
         Get your Dream IIT <br /> With Personalized 1:1 Mentorship
       </h1>
-      <p style={{ fontSize: '1.8rem', color: '#000000', lineHeight: '1rem' }}>
+      <p className='landing-sub-header'>
         1:1 personal mentorship with iitian helps you to achieve your dream IIT
       </p>
 
@@ -22,11 +22,11 @@ const Landing = () => {
         <h2>&nbsp; Rs 200</h2>
       </div>
 
-      <p style={{ color: '#000000', display: 'flex', gap: 10, marginTop: 3 }}>
+      <p className='landing-trail'>
         There's a{' '}
-        <p style={{ color: '#000000', fontWeight: '500' }}>
+        <span style={{ color: '#000000', fontWeight: '500' }}>
           FREE TRIAL SESSION
-        </p>{' '}
+        </span>{' '}
         that you can use to find your perfect match
       </p>
 
@@ -44,7 +44,7 @@ const Landing = () => {
       </div>
 
 
-      <div className="container px-4 py-3 d-flex gap-3 align-items-center" style={{ backgroundColor: '#fff', marginTop: '1rem', borderRadius: '1.2rem' }}>
+      <div className="container px-4 py-3 d-flex gap-3 align-items-center landing-white-bar" style={{ backgroundColor: '#fff', marginTop: '1rem', borderRadius: '1.2rem' }}>
         <div className='d-flex align-items-center justify-content-between landing-numbers'>
           <div className='d-flex align-items-center gap-3'>
             <img src={userFill} alt="" />
@@ -67,7 +67,7 @@ const Landing = () => {
 
           <h3 className='text-center text-uppercase mentor-from' style={{color: '#000'}}>Mentor from <h3 className='d-flex justify-content-center gap-2' style={{color: '#ff691b'}}>TOP IIT <h3 style={{color: '#000'}}>like</h3></h3> </h3>
 
-        <div className='d-flex gap-5 collges'>
+        <div className='d-flex gap-2 gap-md-5 collges'>
           <img width={50} height={50} src={bombay} alt="" />
           <img width={50} height={50} src={madras} alt="" />
           <img width={50} height={50} src={kanpur} alt="" />
