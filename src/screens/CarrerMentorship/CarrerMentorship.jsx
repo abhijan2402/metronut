c2715cb5d29f4164056311a3f079c50b69c63ac6
abhijan2402 <abhishek.jangid643@gmail.com>
2023-06-17 19:30:31 +0530

    Adding some chnages

--- src/screens/CarrerMentorship/CarrerMentorship.jsx
+++ src/screens/CarrerMentorship/CarrerMentorship.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import './CarrerMentorship.css'
 import Session from '../../components/Session/Session'
 import Navbar from '../../components/navbar/Navbar'
+import Works from '../../components/works/Works'
+import Courses from '../../components/Courses/Courses'
 
 const CarrerMentorship = () => {
   return (
     <>
-    <Navbar />
-    <div className='container mt-5'>
+      <Navbar />
+      <div className='container mt-5'>
 
-        <Session/>
-    </div>
+        <Session />
+        <Works />
+        <Courses />
+      </div>
     </>
   )
 }
